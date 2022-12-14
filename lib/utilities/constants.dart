fd745c15c6d87ae40fa3b2eb99ac1c76e1c78099
Y.Tanaka <64997909+ystnk20@users.noreply.github.com>
2022-12-14 16:43:03 +0900

    first commit

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -18,3 +18,16 @@ const kButtonTextStyle = TextStyle(
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+);
